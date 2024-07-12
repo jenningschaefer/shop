9e567444eda791691d335d6c96c5dc448182c9e1
Jenning Schäfer <jenning@swibe.de>
2024-07-12 15:11:19 +0200

    footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,5 +2,48 @@
 </script>
 
 <template>
-    <div class="footer">unten</div>
+    <div class="footer">
+        <div class="footer_links">
+            <NuxtLink to="/" class="footer_links_link">
+                <p class="links">Service</p>
+                <svg class="footer_links_link_icon">
+                    <use href="~/assets/svg/icons.svg#link"/>
+                </svg>
+            </NuxtLink>
+            <NuxtLink to="/" class="footer_links_link">
+                <p class="links">Legal</p>
+                <svg class="footer_links_link_icon">
+                    <use href="~/assets/svg/icons.svg#link"/>
+                </svg>
+            </NuxtLink>
+            <NuxtLink to="/" class="footer_links_link">
+                <p class="links">Payment</p>
+                <svg class="footer_links_link_icon">
+                    <use href="~/assets/svg/icons.svg#link"/>
+                </svg>
+            </NuxtLink>
+            <a href="" class="footer_links_link">
+                <p class="links">Design</p>
+                <svg class="footer_links_link_icon">
+                    <use href="~/assets/svg/icons.svg#link"/>
+                </svg>
+            </a>
+        </div>
+        <div class="footer_newsletter">
+            <div class="subtitle-big">
+                Join Our Newsletter
+            </div>
+            <div class="footer_newsletter_input">
+                <input class="input-dark input-newsletter" type="text" placeholder="E-Mail Address">
+                <svg class="footer_links_link_icon">
+                    <use href="~/assets/svg/icons.svg#link"/>
+                </svg>
+            </div>
+            <div class="footer_newsletter_disclaimer">
+                <small>
+                    *Alle Preise inkl. Mwst., zzgl. anfallender Versand- und Lieferkosten.
+                </small>
+            </div>
+        </div>
+    </div>
 </template>

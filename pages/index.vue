f2463742fe107fd3271c2485e9b2bad8e1f1be98
Jenning Schäfer <jenning@swibe.de>
2024-07-12 16:29:23 +0200

    home

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,48 @@
 </script>
 
 <template>
-  test
+  <div class="home">
+    <!-- slogan -->
+    <div class="home_slogan">
+      <div class="hero-font">make it simple<br>bit significant</div>
+    </div>
+    <!-- slogan end -->
+
+    <!-- categories -->
+    <div class="home_categories">
+      <NuxtLink to="/products/chairs" class="home_categories_link">
+          <p class="links">Chairs</p>
+          <svg class="home_categories_link_icon">
+              <use href="~/assets/svg/icons.svg#link"/>
+          </svg>
+      </NuxtLink>
+      <NuxtLink to="/products/ligting" class="home_categories_link">
+          <p class="links">Lighting</p>
+          <svg class="home_categories_link_icon">
+              <use href="~/assets/svg/icons.svg#link"/>
+          </svg>
+      </NuxtLink>
+      <NuxtLink to="/products/tables" class="home_categories_link">
+          <p class="links">Tables</p>
+          <svg class="home_categories_link_icon">
+              <use href="~/assets/svg/icons.svg#link"/>
+          </svg>
+      </NuxtLink>
+      <NuxtLink to="/products/decoration" class="home_categories_link">
+          <p class="links">Decoration</p>
+          <svg class="home_categories_link_icon">
+              <use href="~/assets/svg/icons.svg#link"/>
+          </svg>
+      </NuxtLink>
+    </div>
+    <!-- categories end -->
+
+    <!-- hero -->
+    <div class="home_hero">
+      <!-- <NuxtImg class="" @click="" src=""/> -->
+    </div>
+    <!-- hero end -->
+  </div>
 </template>
 
  

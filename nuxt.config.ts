c62a3e2de0b8b35db887726759ba74195a73faa8
Jenning Schäfer <jenning@swibe.de>
2024-07-15 17:27:31 +0200

    home responsive

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    "@nuxt/image-edge",
-    "@vueuse/nuxt",
-    '@nuxt/ui',
-  ],
+  modules: ["@nuxt/image-edge", "@vueuse/nuxt", '@nuxt/ui', "@nuxt/image"],
 
   css: ['~/assets/scss/main.scss'],
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,52 +1,53 @@
 <script setup>
-  useHead({
-    title: 'Shop',
-  })
+useHead({
+  title: 'Shop',
+})
 </script>
 
 <template>
   <div class="home">
     <!-- slogan -->
     <div class="home_slogan">
-      <div class="hero-font">make it simple<br>bit significant</div>
+      <svg class="home_slogan_bg">
+        <use href="~/assets/svg/logos.svg#logo-bg" />
+      </svg>
+      <div class="home_slogan_text hero-font">make it simple<br>bit significant</div>
     </div>
     <!-- slogan end -->
 
     <!-- categories -->
     <div class="home_categories">
       <NuxtLink to="/products/chairs" class="home_categories_link">
-          <p class="links">Chairs</p>
-          <svg class="home_categories_link_icon">
-              <use href="~/assets/svg/icons.svg#link"/>
-          </svg>
+        <p class="links">Chairs</p>
+        <svg class="home_categories_link_icon">
+          <use href="~/assets/svg/icons.svg#link" />
+        </svg>
       </NuxtLink>
       <NuxtLink to="/products/ligting" class="home_categories_link">
-          <p class="links">Lighting</p>
-          <svg class="home_categories_link_icon">
-              <use href="~/assets/svg/icons.svg#link"/>
-          </svg>
+        <p class="links">Lighting</p>
+        <svg class="home_categories_link_icon">
+          <use href="~/assets/svg/icons.svg#link" />
+        </svg>
       </NuxtLink>
       <NuxtLink to="/products/tables" class="home_categories_link">
-          <p class="links">Tables</p>
-          <svg class="home_categories_link_icon">
-              <use href="~/assets/svg/icons.svg#link"/>
-          </svg>
+        <p class="links">Tables</p>
+        <svg class="home_categories_link_icon">
+          <use href="~/assets/svg/icons.svg#link" />
+        </svg>
       </NuxtLink>
       <NuxtLink to="/products/decoration" class="home_categories_link">
-          <p class="links">Decoration</p>
-          <svg class="home_categories_link_icon">
-              <use href="~/assets/svg/icons.svg#link"/>
-          </svg>
+        <p class="links">Decoration</p>
+        <svg class="home_categories_link_icon">
+          <use href="~/assets/svg/icons.svg#link" />
+        </svg>
       </NuxtLink>
     </div>
     <!-- categories end -->
 
     <!-- hero -->
     <div class="home_hero">
-      <!-- <NuxtImg class="" @click="" src=""/> -->
+      <NuxtImg class="" @click="" src="/img/pexels-heyho-6077368.jpg" />
     </div>
     <!-- hero end -->
   </div>
 </template>
-
- 

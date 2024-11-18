f5008dc6abe47aca1237d9bc30eb1894eb97c2d1
jenningschaefer <jenningschaefer@googlemail.com>
2024-11-18 13:47:29 +0100

    back on products

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,22 +1,24 @@
 <script setup>
+const router = useRouter();
+const route = useRoute();
 
 const subnavIcons = ref([
     {
         svg: "~/assets/svg/icons.svg#lang-de",
         click: "",
-    },    
+    },
     {
         svg: "~/assets/svg/icons.svg#cart",
         click: "",
-    },    
+    },
     {
         svg: "~/assets/svg/icons.svg#heart",
         click: "",
-    },    
+    },
     {
         svg: "assets/svg/icons.svg#user",
         click: "",
-    },     
+    },
 ]);
 
 
@@ -27,56 +29,74 @@ const subnavIcons = ref([
         <div class="navBar">
             <NuxtLink to="/" class="navBar_home">
                 <svg class="navBar_logo">
-                    <use href="~/assets/svg/logos.svg#logo"/>
+                    <use href="~/assets/svg/logos.svg#logo" />
                 </svg>
             </NuxtLink>
             <div class="navBar_icons">
+                <template v-if="route.name === 'products-name-id'">
+                    <a @click="router.back()">
+                        <svg class="subNav_icon">
+                            <use href="~/assets/svg/icons.svg#arrow-left" />
+                        </svg>
+                    </a>
+                </template>
+                <template v-else>
+                    <a @click="">
+                        <svg class="subNav_icon">
+                            <use href="~/assets/svg/icons.svg#lang-de" />
+                        </svg>
+                    </a>
+                </template>
                 <a @click="">
                     <svg class="navBar_icons_icon">
-                        <use href="~/assets/svg/icons.svg#lang-de"/>
+                        <use href="~/assets/svg/icons.svg#cart" />
                     </svg>
                 </a>
                 <a @click="">
                     <svg class="navBar_icons_icon">
-                        <use href="~/assets/svg/icons.svg#cart"/>
+                        <use href="~/assets/svg/icons.svg#heart" />
                     </svg>
                 </a>
                 <a @click="">
                     <svg class="navBar_icons_icon">
-                        <use href="~/assets/svg/icons.svg#heart"/>
-                    </svg>
-                </a>
-                <a @click="">
-                    <svg class="navBar_icons_icon">
-                        <use href="~/assets/svg/icons.svg#user"/>
+                        <use href="~/assets/svg/icons.svg#user" />
                     </svg>
                 </a>
             </div>
             <div class="navBar_menu">
-                <a >menu</a>
+                <a>menu</a>
             </div>
         </div>
         <div class="subNav">
+            <template v-if="route.name === 'products-name-id'">
+                <a @click="router.back()">
+                    <svg class="subNav_icon">
+                        <use href="~/assets/svg/icons.svg#arrow-left" />
+                    </svg>
+                </a>
+            </template>
+            <template v-else>
+                <a @click="">
+                    <svg class="subNav_icon">
+                        <use href="~/assets/svg/icons.svg#lang-de" />
+                    </svg>
+                </a>
+            </template>
             <a @click="">
-                    <svg class="subNav_icon">
-                        <use href="~/assets/svg/icons.svg#lang-de"/>
-                    </svg>
-                </a>
-                <a @click="">
-                    <svg class="subNav_icon">
-                        <use href="~/assets/svg/icons.svg#cart"/>
-                    </svg>
-                </a>
-                <a @click="">
-                    <svg class="subNav_icon">
-                        <use href="~/assets/svg/icons.svg#heart"/>
-                    </svg>
-                </a>
-                <a @click="">
-                    <svg class="subNav_icon">
-                        <use href="~/assets/svg/icons.svg#user"/>
-                    </svg>
-                </a>
+                <svg class="subNav_icon">
+                    <use href="~/assets/svg/icons.svg#cart" />
+                </svg>
+            </a>
+            <a @click="">
+                <svg class="subNav_icon">
+                    <use href="~/assets/svg/icons.svg#heart" />
+                </svg>
+            </a>
+            <a @click="">
+                <svg class="subNav_icon">
+                    <use href="~/assets/svg/icons.svg#user" />
+                </svg>
+            </a>
         </div>
     </div>
 </template>

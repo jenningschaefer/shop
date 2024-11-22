4019bf0c17b7a444227fae376ba3ecefdaa93266
jenningschaefer <jenningschaefer@googlemail.com>
2024-11-22 18:28:21 +0100

    tabs element

---
+++ components/UI/tab-item.vue
@@ -0,0 +1,9 @@
+<script setup>
+  const props = defineProps([ 'active' ]);
+</script>
+
+<template>
+  <div class="tab" :class="(active == 'true') ? 'active' : ''" ref="tabs">
+    <slot></slot>
+  </div>
+</template>

fdcdce0445c871cf6a6cfc6889a8ac3bab402f3e
jenningschaefer <jenningschaefer@googlemail.com>
2024-11-24 20:44:06 +0100

    payment mobile

--- pages/checkout/payment.vue
+++ pages/checkout/payment.vue
@@ -2,10 +2,33 @@
 definePageMeta({
     layout: 'checkout',
 });
+
+const paymentMethods = [
+    {
+        name: "Paypal",
+        desciption: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+    },
+    {
+        name: "Credit Card",
+        desciption: "We accept all major credit cards, including Visa, MasterCard, American Express, and Discover. Your information is securely encrypted and protected for a safe and seamless transaction experience. For any assistance or inquiries, our customer support team is available 24/7."
+    },
+    {
+        name: "Apple/Google Pay",
+        desciption: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."
+    },
+];
+
+const paymentIndex = ref(0);
 </script>
 
 <template>
     <div class="payment">
         <h1 class="checkout_title">Payment</h1>
+        <div class="payment_methods">
+            <template v-for="(payment, index) in paymentMethods" :key="index">
+                <div class="payment_methods-method" :class="paymentIndex == index ? 'active' : ''"
+                    @click="paymentIndex = index">{{ payment.name }}</div>
+            </template>
+        </div>
     </div>
 </template>

a7a01266aa4146c8e4d676c487da77c8dc4c5429
jenningschaefer <jenningschaefer@googlemail.com>
2024-11-25 17:20:24 +0100

    confirmation and overview mobile

--- pages/checkout/confirmation.vue
+++ pages/checkout/confirmation.vue
@@ -5,7 +5,25 @@ definePageMeta({
 </script>
 
 <template>
-    <div class="confirmatiion">
-        confirmation
+    <div class="confirmation">
+        <div class="confirmation_logo">
+            <NuxtLink to="/">
+                <svg>
+                    <use href="~/assets/svg/logos.svg#logo" />
+                </svg>
+            </NuxtLink>
+        </div>
+        <div class="confirmation_success">
+            <div class="confirmation_success_panel">
+                <svg class="confirmation_success_panel-icon">
+                    <use href="~/assets/svg/icons.svg#check" />
+                </svg>
+                <div class="confirmation_success_panel-title">Success!!</div>
+                <div class="confirmation_success_panel-text">Your Order has<br> been confirmed</div>
+                <NuxtLink to="/" class="confirmation_success_panel-button">
+                    <button class="dark-btn-socondary confirmation_success_panel-button" type="button">home</button>
+                </NuxtLink>
+            </div>
+        </div>
     </div>
 </template>

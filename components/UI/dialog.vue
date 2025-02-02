22a0375f8d40509a9523317c6c90839eef5f1d00
jenningschaefer <jenningschaefer@googlemail.com>
2025-02-02 17:21:21 +0100

    dialog component

---
+++ components/UI/dialog.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+const active = defineModel();
+
+</script>
+
+<template>
+    <div v-if="active" class="overlay"></div>
+    <div v-if="active" class="dialog">
+        <div class="dialog_header">
+            <slot name="title"></slot>
+            <a class="closebtn" @click="active = false">
+                <svg>
+                    <use href="~/assets/svg/icons.svg#close" />
+                </svg>
+            </a>
+        </div>
+        <div class="dialog_content">
+            <slot name="content"></slot>
+        </div>
+        <div class="dialog_footer">
+            <slot name="footer"></slot>
+        </div>
+    </div>
+</template>

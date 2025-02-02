c16a11178d9222f7fc1bb8a30148dc49dbb923e6
jenningschaefer <jenningschaefer@googlemail.com>
2025-02-02 18:44:28 +0100

    deploy

--- .output/public/_nuxt/CkGfkPZu.js
+++
@@ -1,1 +0,0 @@
-import"./CRqW0QVJ.js";const r=""+new URL("logos.BDEyHZpf.svg",import.meta.url).href;export{r as _};
--- .output/server/chunks/build/logos-CBvRb9s_.mjs
+++
@@ -1,6 +0,0 @@
-import { a as buildAssetsURL } from '../routes/renderer.mjs';
-
-const _imports_0 = "" + buildAssetsURL("logos.BDEyHZpf.svg");
-
-export { _imports_0 as _ };
-//# sourceMappingURL=logos-CBvRb9s_.mjs.map
